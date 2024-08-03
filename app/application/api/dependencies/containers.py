a16921b14515c9115d36ee4f7dabddb43d4380d2
sdfy-fly <rozhov25@bk.ru>
2024-08-03 23:59:33 +0400

    added dependency injector

---
+++ app/application/api/dependencies/containers.py
@@ -0,0 +1,5 @@
+from punq import Container
+
+from app.logic.init import init_container
+
+container: Container = init_container()
--- app/logic/init.py
+++ app/logic/init.py
@@ -1,15 +1,29 @@
-from app.infra.repositories.chat import BaseChatRepository
+from functools import lru_cache
+
+from punq import Container
+
+from app.infra.repositories.chat import BaseChatRepository, MemoryChatRepository
 from app.logic.commands.chat import CreateChatCommand, CreateChatCommandHandler
 from app.logic.mediator import Mediator
 
 
-def init_mediator(
-        mediator: Mediator,
-        chat_repository: BaseChatRepository
-):
-    mediator.register_command(
-        command=CreateChatCommand,
-        command_handlers=[
-            CreateChatCommandHandler(repository=chat_repository)
-        ]
-    )
+@lru_cache(1)
+def init_container() -> Container:
+    container = Container()
+    container.register(BaseChatRepository, MemoryChatRepository)
+    container.register(CreateChatCommandHandler)
+
+    def init_mediator():
+        mediator = Mediator()
+        mediator.register_command(
+            command=CreateChatCommand,
+            command_handlers=[
+                container.resolve(CreateChatCommandHandler)
+            ]
+        )
+
+        return mediator
+
+    container.register(Mediator, factory=init_mediator)
+
+    return container
